cb696022b2acd808c2fc4d588cc7c77f1eb7b8c5
aSmecta <jiaoxiangpo2007@live.cn>
2017-03-01 15:35:55 +0800

    更新版本

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 /**
  * Created by Administrator on 2015-10-19.
  */
+'use strict';
 
 var webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until;
+    By = webdriver.By;
 var driver = new webdriver.Builder()
     .forBrowser('chrome')
-    // .setChromeLogFile('C:\Users\jiaox\Documents\new\log\log.txt')
-    // .enableVerboseLogging()
     .build();
 
-driver.get('http://www.mazgo.net:12980/admin');
+//服务器信息
+driver.get('http://www.sunagy.com:9999/Sa');
 
 //登录信息
-// var email='srcd@srcd.net';
-var email='asmecta@sunagy.com';
-var password='111111';
+var email=[
+    'asmecta@sunagy.com',       //0
+    'james.zhang@sunagy.com',   //1
+    'test05@sunagy.com',        //2
+    'test06@a.com',             //3
+    'b01@b.com',                //4
+    'eric@qq.com'               //5
+];
 
-driver.findElement(By.id('email')).sendKeys(email);
-driver.findElement(By.id('password')).sendKeys(password);
+driver.findElement(By.id('email')).sendKeys(email[2]);
+driver.findElement(By.id('password')).sendKeys('111111');
 console.log(driver.getTitle());
 driver.findElement(By.css('#login')).click();
